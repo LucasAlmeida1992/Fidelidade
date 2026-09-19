62b59547755e70593ac0175dabcc9d9c59e2b585
Lucas Almeida <lucas.contato07@hotmail.com>
2026-09-19 18:44:14 -0300

    link api

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // ==========================================
 
 // Substitua pela URL do seu Web App implantado no Google Sheets
-const API_URL = "COLOQUE_SUA_URL_DO_APPS_SCRIPT_AQUI";
+const API_URL = "https://script.google.com/macros/s/AKfycbz-ohIZL5xvFQvPSgtvJw8Js2S71lCW00kFGcmj3TcymFucYbonbNrNRx_Fn7vpdDYF/exec";
 
 // SVGs Configuráveis (Herdam a cor e tamanho do CSS)
 const SVG_CHECK = `<svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="currentColor"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/></svg>`;
